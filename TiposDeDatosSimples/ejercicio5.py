6388db1fd8f52a51fcc4f25ac187267444238a2b
Francisco Uribe <furibearanda@gmail.com>
2026-09-23 13:43:41 +0200

    5

---
+++ TiposDeDatosSimples/ejercicio5.py
@@ -0,0 +1,4 @@
+Horas= input("Introduce las horas trabajadas: ")
+coste= input("Introduce el coste por hora: ")
+total= int(Horas) * float(coste)
+print(f"lo que te tienen que pagar es : {total} euros")
