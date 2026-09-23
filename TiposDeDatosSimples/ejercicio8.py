52d313d3a4a8c1559f129739b755923c90dde194
Francisco Uribe <furibearanda@gmail.com>
2026-09-23 14:04:07 +0200

    8

---
+++ TiposDeDatosSimples/ejercicio8.py
@@ -0,0 +1,6 @@
+n1= int(input("Introduce el primer número: "))
+n2= int(input("Introduce el segundo número: "))
+c = n1 // n2
+r = n1 % n2
+
+print(f"{n1} entre {n2} da un cociente {c} y un resto {r}")
