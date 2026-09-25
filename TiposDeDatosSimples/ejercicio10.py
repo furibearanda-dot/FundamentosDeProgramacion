1dacddaaeebeeb4f50fa6730a2c50cdd81381f5e
Francisco Uribe <furibearanda@gmail.com>
2026-09-25 11:55:16 +0200

    10

---
+++ TiposDeDatosSimples/ejercicio10.py
@@ -0,0 +1,6 @@
+Npayasos= int(input("Ingrese el número de payasos: "))
+Nmuñecas= int(input("Ingrese el número de muñecas: "))
+pesoPayaso= 112
+pesoMuñeca= 75
+pesoTotal= (Npayasos * pesoPayaso) + (Nmuñecas * pesoMuñeca)
+print(f"El peso total del paquete es: {pesoTotal} gramos")
