334cf4dcc44e51e2f2fb33e3e4eab3f4e037ff5f
Francisco Uribe <furibearanda@gmail.com>
2026-09-25 14:35:51 +0200

    12+

--- TiposDeDatosSimples/ejercicio12.py
+++ TiposDeDatosSimples/ejercicio12.py
@@ -1,3 +1,3 @@
 barrasNuevas= 3.49
 barrasViejas= barrasNuevas * 0.60
-print(f"El precio de las barras nuevas es: {barrasNuevas} euros, y el precio de las barras de ayer es {barrasViejas} euros")
+print(f"El precio de las barras nuevas es: {barrasNuevas} euros, y el precio de las barras de ayer tiene un 60% de descuento, osea el precio es {barrasViejas} euros")
